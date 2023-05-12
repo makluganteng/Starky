d5f60e02f4fd9bbea8038a255528015ab1f25dff
V.O.T <makluganteng11@gmail.com>
2023-05-12 14:13:03 +0700

    fix to get some of the block data

--- component/src/block.rs
+++ component/src/block.rs
@@ -1,35 +1,40 @@
-use ethers::{types::U256, prelude::k256::elliptic_curve::Field};
+use ethers::{prelude::k256::elliptic_curve::Field, types::U256};
 use starknet::core::types::{FieldElement, FromStrError};
 use thiserror::Error;
-struct Block {
-    block_number: u32,
-    block_hash: FieldElement,
+pub struct BlockDisplay {
+    block_number: Option<u64>,
+    block_hash: Option<FieldElement>,
     number_txn: u32,
     number_messages: u32,
-    number_events: u32 
+    number_events: u32,
 }
-
 
 pub type Result<T> = std::result::Result<T, FromStrError>;
 
-impl Block {
+impl BlockDisplay {
     //constructor when a new block is being recieved from the block chain.
-    pub fn new(block_number: u32, block_hash: FieldElement, number_txn: u32, number_messages: u32, number_events: u32) -> Self{
+    pub fn new(
+        block_number: Option<u64>,
+        block_hash: Option<FieldElement>,
+        number_txn: u32,
+        number_messages: u32,
+        number_events: u32,
+    ) -> Self {
         Self {
             block_number,
             block_hash,
             number_txn,
             number_messages,
-            number_events
+            number_events,
         }
     }
 
     //get methods
-    pub fn get_block_number(&self) -> u32 {
+    pub fn get_block_number(&self) -> Option<u64> {
         self.block_number
-    }  
+    }
 
-    pub fn get_block_hash(&self) -> FieldElement {
+    pub fn get_block_hash(&self) -> Option<FieldElement> {
         self.block_hash
     }
 
@@ -48,12 +53,16 @@ impl Block {
 
 mod test {
     use super::*;
+    // Assuming FieldElement is defined in the same module
+
     #[test]
-    fn new_block(){
+    fn new_block() {
         let hash = FieldElement::from_hex_be(
-            "049ee3eba8c1600700ee1b87eb599f16716b0b1022947733551fde4050ca6805").unwrap();
-        let block = Block::new(48872,hash, 222, 5, 1055);
-        assert_eq!(block.get_block_number(),48872);
-        assert_eq!(block.get_block_hash(),hash);
+            "049ee3eba8c1600700ee1b87eb599f16716b0b1022947733551fde4050ca6805",
+        )
+        .unwrap();
+        let block = BlockDisplay::new(Some(48872), Some(hash), 222, 5, 1055);
+        assert_eq!(block.get_block_number(), Some(48872));
+        assert_eq!(block.get_block_hash(), Some(hash));
     }
 }
